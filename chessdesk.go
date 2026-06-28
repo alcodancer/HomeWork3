342567bb9cb3fc262893aabeeb39f0cd038bc949
alcodancer <138016557+alcodancer@users.noreply.github.com>
2026-06-28 11:36:56 +0300

    Update chessdesk.go

--- chessdesk.go
+++ chessdesk.go
@@ -1,17 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+var chess_size = 8
 
 func main() {
-	var chess_size int
-	chess_size = 8
+	draw_desk()
+}
+
+func draw_desk() {
 	for i := 1; i <= chess_size; i++ {
-		for j := 1; j <= chess_size/2; j++ {
-			if i%2 == 0 {
-				fmt.Print("# ")
+		fmt.Printf("%d ", i)
+		for j := 1; j <= chess_size; j++ {
+			if j%2 == 0 && i%2 == 1 {
+				fmt.Print("▒")
+				fmt.Print("|")
 			}
-			if i%2 == 1 {
-				fmt.Print(" #")
+			if j%2 == 1 && i%2 == 1 {
+				fmt.Print(" ")
+				fmt.Print("|")
+			}
+			if j%2 == 1 && i%2 == 0 {
+				fmt.Print("▒")
+				fmt.Print("|")
+			}
+			if j%2 == 0 && i%2 == 0 {
+				fmt.Print(" ")
+				fmt.Print("|")
 			}
 		}
 		fmt.Println("")
