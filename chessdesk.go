cbfb6e233e2faf763c5adc9cb1b1b4a1c00a5842
alcodancer <138016557+alcodancer@users.noreply.github.com>
2026-06-30 12:09:57 +0300

    Update chessdesk.go

--- chessdesk.go
+++ chessdesk.go
@@ -4,32 +4,21 @@ import (
 	"fmt"
 )
 
-var chess_size = 8
+var chess_size = 7
 
 func main() {
 	draw_desk()
 }
-
 func draw_desk() {
+	k := " |▒|"
 	for i := 1; i <= chess_size; i++ {
-		fmt.Printf("%d ", i)
-		for j := 1; j <= chess_size; j++ {
-			if j%2 == 0 && i%2 == 1 {
-				fmt.Print("▒")
-				fmt.Print("|")
-			}
-			if j%2 == 1 && i%2 == 1 {
-				fmt.Print(" ")
-				fmt.Print("|")
-			}
-			if j%2 == 1 && i%2 == 0 {
-				fmt.Print("▒")
-				fmt.Print("|")
-			}
-			if j%2 == 0 && i%2 == 0 {
-				fmt.Print(" ")
-				fmt.Print("|")
-			}
+		if k == "▒| |" {
+			k = " |▒|"
+		} else {
+			k = "▒| |"
+		}
+		for j := 1; j <= chess_size/2; j++ {
+			fmt.Print(k)
 		}
 		fmt.Println("")
 	}
